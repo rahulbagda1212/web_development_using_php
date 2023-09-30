749389150199e238a06930d6094cf5a8122ed03f
rahulbagda1212 <143157089+rahulbagda1212@users.noreply.github.com>
2023-09-30 13:17:40 +0530

    Delete task2 directory

--- task2/Script_12.php
+++
@@ -1,7 +0,0 @@
-<?php
-$str1="Rahul";
-$str2="Bagda";
-$str3=$str1.$str2;
-echo " The concat the str1 and str2 is $str3 \n";
-
-?>
--- task2/Script_13.php
+++
@@ -1,4 +0,0 @@
-<?php
-$c=150;
-echo "The Fahrenheit is ".($c*9/5)+32;
-?>
